1d6787914a198a5b5232111cca9c4d2f02a7f524
tatsuki199 <tanukityomari@gmail.com>
2024-11-25 23:09:13 +0900

    コメント機能修正完了

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,11 @@
 class CommentsController < ApplicationController
   def create
     Comment.create(comment_params)
-    
+    redirect_to post_path(params[:post_id])
   end
 
   private
   def comment_params
-    params.require(:comment).permit(:answer, :name)
+    params.require(:comment).permit(:answer, :name).merge(post_id: params[:post_id])
   end
 end
